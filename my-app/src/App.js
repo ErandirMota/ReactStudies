867116d986bc03c69ad376aef7b9c7be8aa33412
unknown <erandirsm13@gmail.com>
2023-11-30 23:24:27 -0300

    calculator using react hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-import Botao from './Botao';
 
-function App() {
+export default function App() {
+  const [firstNumber, setFirstNumber] = useState(0);
+  const [secondNumber, setSecondNumber] = useState(0);
+  const [result, setResult] = useState(0);
+
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Botao></Botao>
-          <p>Meu primeiro projeto</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>Calculator</h1>
+      <div className="card">
+        <label>
+          First Number
+          <input
+            value={firstNumber}
+            onChange={(e) => setFirstNumber(Number(e.target.value))}
+          />
+        </label>
+        <label>
+          Second Number
+          <input
+            value={secondNumber}
+            onChange={(e) => setSecondNumber(Number(e.target.value))}
+          />
+        </label>
+        <span>Result: {result}</span>
       </div>
-      <div>asdf</div>
+      <button onClick={() => setResult(firstNumber + secondNumber)}>+</button>
+      <button onClick={() => setResult(firstNumber - secondNumber)}>-</button>
+      <button onClick={() => setResult(firstNumber * secondNumber)}>*</button>
+      <button onClick={() => setResult(firstNumber / secondNumber)}>/</button>
     </>
-  )
+  );
 }
 
-export default App;
+
--- my-app/src/Botao.js
+++
@@ -1,5 +0,0 @@
-const Botao = () => {
-  return (<button onClick={() => alert('Clicado')}>Me clique</button>)
-};
-
-export default Botao;
